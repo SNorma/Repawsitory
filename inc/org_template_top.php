786763996540b66b612a8d90e457648576c46e21
SNorma <yare_93@icloud.com>
2018-10-20 15:35:40 -0700

    Add files via upload

---
+++ inc/org_template_top.php
@@ -0,0 +1,27 @@
+<?php
+    include_once("php_includes/check_login_status.php");
+?>
+
+<div id="pageTop">
+  <div id="pageTopWrap">
+    <div id="pageTopLogo">
+    </div>
+    <div id="pageTopRest">
+      <div id="menu1">
+        <div>
+            <?php 
+                if($user_ok == true){
+                    echo '<a href="../editaccount.php?u=' . $_SESSION["username"] . '"> Edit Account </a>';
+                }
+                else {
+                    echo '<a href="index.php"> Home </a> | <a href="login.php">Login </a> |  <a href="signup.php">Sign Up</a>';
+                }
+            ?>
+        </div>
+      </div>
+        <div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
